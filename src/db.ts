f2f3c75b83dd439e52ca5e7e49972f5e68ab02b3
hazel97 <omarmattia97@gmail.com>
2022-11-04 21:48:27 +0200

    refactor: Added default port value to db client

--- src/db.ts
+++ src/db.ts
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const [DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT] = [
+const [DB_USERNAME, DB_PASSWORD, DB_NAME] = [
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  process.env.DB_NAME,
-  process.env.DB_PORT,
+  process.env.DB_NAME
 ];
+
+let DB_PORT = process.env.DB_PORT;
+if(DB_PORT === undefined) DB_PORT = '5432';
 
 const dbClient = new Pool({
   user: DB_USERNAME,
