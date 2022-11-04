56bbdcc9a51db2d7d318b2cfc8010a55f3c8d8cf
hazel97 <omarmattia97@gmail.com>
2022-11-04 20:31:13 +0200

    feat: Added DB client module

---
+++ src/db.ts
@@ -0,0 +1,20 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const [DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT] = [
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_PORT,
+];
+
+const dbClient = new Pool({
+  user: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: Number(DB_PORT)
+});
+
+export default dbClient;
