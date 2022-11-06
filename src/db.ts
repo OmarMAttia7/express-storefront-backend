d98fccd11e3c1c364e24741b11b0f2ac57bdad86
hazel97 <omarmattia97@gmail.com>
2022-11-06 21:36:10 +0200

    fix: dbclient now reads ENV correctly

--- src/db.ts
+++ src/db.ts
@@ -3,16 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const [DB_USERNAME, DB_PASSWORD, DB_NAME] = [
+const [DB_USERNAME, DB_PASSWORD, ENV] = [
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
-  process.env.DB_NAME
+  process.env.ENV,
 ];
 
+// Get the port from environment variable
+// if it doesn't exist default to port 5432
 let DB_PORT = process.env.DB_PORT;
-if(DB_PORT === undefined) DB_PORT = '5432';
+if (DB_PORT === undefined) DB_PORT = "5432";
 
-const dbClient = new Pool({
+const DB_NAME = ENV === "test" ? process.env.TEST_DB_NAME : process.env.DB_NAME;
+
+const dbClient: Pool = new Pool({
   user: DB_USERNAME,
   password: DB_PASSWORD,
   database: DB_NAME,
