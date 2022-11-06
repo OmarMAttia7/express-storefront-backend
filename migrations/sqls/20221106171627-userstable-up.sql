755ad940709a2806b41a6b69cacbebb3020dbe63
hazel97 <omarmattia97@gmail.com>
2022-11-06 21:34:56 +0200

    feat: Added users table & model

---
+++ migrations/sqls/20221106171627-userstable-up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users 
+(
+id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+password_digest CHAR(60)
+);
---
+++ src/models/users.ts
@@ -0,0 +1,5 @@
+class Users {
+
+}
+
+export default Users;
