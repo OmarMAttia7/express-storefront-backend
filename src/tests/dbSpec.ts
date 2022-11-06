706dc73972a77a5ec396dd56d1053a320d8ef6a4
hazel97 <omarmattia97@gmail.com>
2022-11-06 16:47:08 +0200

    test: added database testing

--- src/tests/dbSpec.ts
+++ src/tests/dbSpec.ts
@@ -6,8 +6,6 @@ describe("Database Client", () => {
   });
 
   it("successfully connects to database", async () => {
-    // The value of testMsg doesn't matter, it's purpose is to make sure the
-    // test checks for the same string
     const testString = "connected";
     const res = await dbClient.query("SELECT $1::text as message", [
       testString,
