cf2d484438ff7969f3f24484f51630904b9bfe1d
hazel97 <omarmattia97@gmail.com>
2022-11-07 22:35:25 +0200

    refactor: refactored db module

--- src/db.ts
+++ src/db.ts
@@ -1,26 +1,25 @@
 import { Pool } from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
+import env from "./utils/env";
 
 const [DB_USERNAME, DB_PASSWORD, ENV] = [
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  process.env.ENV,
+  env('DB_USERNAME'),
+  env('DB_PASSWORD'),
+  env('ENV'),
 ];
 
 // Get the port from environment variable
 // if it doesn't exist default to port 5432
-let DB_PORT = process.env.DB_PORT;
+let DB_PORT = env('DB_PORT');
 if (DB_PORT === undefined) DB_PORT = "5432";
 
-const DB_NAME = ENV === "test" ? process.env.TEST_DB_NAME : process.env.DB_NAME;
+// Get the database name according to current environment
+const DB_NAME = ENV === "test" ? env('TEST_DB_NAME') : env('DB_NAME');
 
-const dbClient: Pool = new Pool({
+const dbPool: Pool = new Pool({
   user: DB_USERNAME,
   password: DB_PASSWORD,
   database: DB_NAME,
   port: Number(DB_PORT),
 });
 
-export default dbClient;
+export default dbPool;
