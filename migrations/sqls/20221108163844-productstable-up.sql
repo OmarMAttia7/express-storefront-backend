c3d26c5aa8fa43e1be0d0e77044a33954c08f123
hazel97 <omarmattia97@gmail.com>
2022-11-08 18:52:29 +0200

    chore: Fixed products migration

--- migrations/sqls/20221108163844-productstable-up.sql
+++ migrations/sqls/20221108163844-productstable-up.sql
@@ -3,6 +3,12 @@ CREATE TABLE products
   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   product_name TEXT,
   magnified_price INTEGER,
-  currency_code FOREIGN KEY REFERENCES currencies(code),
-  category_id FOREIGN KEY REFERENCES categories(id)
+  currency_code CHAR(3),
+  category_id INTEGER,
+  CONSTRAINT fk_currency
+   FOREIGN KEY(currency_code)
+    REFERENCES currencies(code),
+  CONSTRAINT fk_category
+    FOREIGN KEY(category_id)
+      REFERENCES categories(id)
 );
