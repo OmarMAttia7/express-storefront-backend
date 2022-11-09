224d15faecc785b800dd0d070e2a1ee821a40955
hazel97 <omarmattia97@gmail.com>
2022-11-09 20:13:16 +0200

    test: Refactored db client test

--- src/tests/dbSpec.ts
+++ src/tests/dbSpec.ts
@@ -3,13 +3,13 @@ import dbPool from "../db";
 function testSuite(): void {
   describe("Database Client", () => {
     it("successfully connects to database", async () => {
-      const dbClient = await dbPool.connect();
+      
       const testString = "connected";
-      const res = await dbClient.query("SELECT $1::text as message", [
+      const res = await dbPool.query("SELECT $1::text as message", [
         testString,
       ]);
       expect(res.rows[0].message).toEqual(testString);
-      dbClient.release();
+      
     });
   });  
 }
