1f11419741c13fa4d7d334567b0b5c2f6f59b54d
hazel97 <omarmattia97@gmail.com>
2022-11-09 20:10:33 +0200

    test: Added currencies model test

---
+++ src/models/tests/currenciesSpec.ts
@@ -0,0 +1,20 @@
+import Currencies from "../currencies";
+
+function testSuite(): void {
+
+  describe("Currencies Model", () => {
+    const currenciesModel = new Currencies();
+
+    it("has an index() method that returns all currencies", async () => {
+      expect(await currenciesModel.index()).toEqual([{currency_code: "USD", magnifier: 100}]);
+    });
+
+    it("has a show() method that returns a currency by code", async () => {
+      expect(await currenciesModel.show("USD")).toEqual({currency_code: "USD", magnifier: 100});
+    });
+
+  });
+
+}
+
+export default testSuite;
