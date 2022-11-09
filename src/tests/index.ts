29354c8da41c2f11412bad5b49a28ae67668b4a1
hazel97 <omarmattia97@gmail.com>
2022-11-09 20:13:02 +0200

    test: Added seeding for tests

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,9 +1,13 @@
 import dbSuite from './dbSpec';
 import modelsSuite from '../models/tests';
 import dbPool from '../db';
+import seed from './test_utils/seedUtil';
 
 describe("Application Testing", () => {
-  
+  beforeAll(async () => {
+    await seed();
+  });
+
   dbSuite();
   modelsSuite();
   
---
+++ src/tests/test_utils/seedUtil.ts
@@ -0,0 +1,7 @@
+import dbPool from "../../db";
+
+async function seed(): Promise<void> {
+  await dbPool.query("INSERT INTO currencies (currency_code, magnifier) VALUES ('USD', 100);");
+}
+
+export default seed;
