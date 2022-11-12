d1e1120f67e50d6df25c7d3c8aa51de2036eced3
hazel97 <omarmattia97@gmail.com>
2022-11-12 21:47:26 +0200

    test: Fixed incorrect test states

--- src/routes/tests/index.ts
+++ src/routes/tests/index.ts
@@ -1,19 +1,7 @@
 import productsSuite from "./products";
-import {
-  setUpProudctsRoute,
-  tearDownProductsRoute,
-} from "./test_utils/productsUtil";
 function testSuite(): void {
   describe("Routes :", () => {
-    beforeAll(async () => {
-      await setUpProudctsRoute();
-    });
-
     productsSuite();
-
-    afterAll(async () => {
-      await tearDownProductsRoute();
-    });
   });
 }
 
--- src/routes/tests/products/index.ts
+++ src/routes/tests/products/index.ts
@@ -1,11 +1,24 @@
 import getProductsSuite from "./getProductsSpec";
 import getProductByIdSuite from "./getProductByIdSpec";
+import createProductSpec from "./createProductSpec";
+import {
+  setUpProudctsRoute,
+  tearDownProductsRoute,
+} from "../test_utils/productsUtil";
+
 function testSuite(): void {
   describe("Products Routes :", () => {
-    
+    beforeEach(async () => {
+      await setUpProudctsRoute();
+    });
+
     getProductsSuite();
     getProductByIdSuite();
+    createProductSpec();
 
+    afterEach(async () => {
+      await tearDownProductsRoute();
+    });
   });
 }
 
