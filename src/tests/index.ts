4125c5d433c238f2dd95d4b6ddb296be086d6737
hazel97 <omarmattia97@gmail.com>
2022-11-12 21:19:53 +0200

    test: Fixed main test suite name

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,15 +1,17 @@
 import dbSuite from "./dbSpec";
 import modelsSuite from "../models/tests";
+import routesSuite from "../routes/tests";
 import dbPool from "../db";
 import seed from "./test_utils/seedUtil";
 
-describe("Application Testing", () => {
+describe("Application Testing :", () => {
   beforeAll(async () => {
     await seed();
   });
 
   dbSuite();
   modelsSuite();
+  routesSuite();
 
   afterAll(async () => {
     await dbPool.end();
