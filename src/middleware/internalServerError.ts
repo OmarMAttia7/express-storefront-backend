fb77772a1344ae4845621590f274d6bfe3130f69
hazel97 <omarmattia97@gmail.com>
2022-11-12 21:19:29 +0200

    feat: Error 500 middleware

---
+++ src/middleware/internalServerError.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export default function(_req: Request, res: Response): Response {
+  return res.status(500).json({
+    error: "Error 500: Internal server error."
+  })
+}
