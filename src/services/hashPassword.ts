36b95fff2aebbbc39825ad6e4c71fcba369587af
hazel97 <omarmattia97@gmail.com>
2022-11-14 02:20:55 +0200

    refactor: Updated hashing function

--- src/services/hashPassword.ts
+++ src/services/hashPassword.ts
@@ -2,7 +2,9 @@ import bcrypt from "bcrypt";
 import env from "../utils/env";
 
 export default async function hashPassword(pwd: string): Promise<string> {
-  const hashedPassword = await bcrypt.hash(pwd, Number(env("SALT_ROUNDS")));
+  const bcryptSecret = env("BCRYPT_SECRET");
+  if(bcryptSecret === undefined) throw Error("missing env variable");
+  const hashedPassword = await bcrypt.hash(bcryptSecret + pwd, Number(env("SALT_ROUNDS")));
 
   return hashedPassword;
 }
