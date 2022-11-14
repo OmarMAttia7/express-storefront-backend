344e355ca36097abf05bf19e9c09656f1e079c45
hazel97 <omarmattia97@gmail.com>
2022-11-14 16:34:38 +0200

    test: Refactored test login function

--- src/routes/tests/test_utils/getJwtToken.ts
+++ src/routes/tests/test_utils/getJwtToken.ts
@@ -2,14 +2,14 @@ import supertest from "supertest";
 import app from "../../../app";
 import { testUsers } from "../../../models/tests/test_utils/testUsersUtil";
 import env from "../../../utils/env";
-async function getJwtToken(id?: number): Promise<string> {
+async function getJwtToken(id: number = 1): Promise<string> {
 try {
   const jwtSecret = env("JWT_SECRET");
   if (jwtSecret === undefined) throw Error("JWT_SECRET not found");
 
   const response = await supertest(app).post("/users/login").send({
-    email: testUsers[0][3],
-    password: testUsers[0][2]
+    email: testUsers[id-1][3],
+    password: testUsers[id-1][2]
   });
 
   const token = response.body.token as string;
