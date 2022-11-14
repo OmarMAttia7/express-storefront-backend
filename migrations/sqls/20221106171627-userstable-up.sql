eca24f5cd718a52a1e30199ecb11ada660d3f4bd
hazel97 <omarmattia97@gmail.com>
2022-11-14 15:10:38 +0200

    chore: Updated migrations

--- migrations/sqls/20221106171627-userstable-up.sql
+++ migrations/sqls/20221106171627-userstable-up.sql
@@ -1,8 +1,8 @@
-CREATE TABLE users 
-(
+CREATE TABLE users (
   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   first_name VARCHAR(50),
   last_name VARCHAR(50),
   email TEXT,
-  password_digest CHAR(60)
+  password_digest CHAR(60),
+  UNIQUE (email)
 );
