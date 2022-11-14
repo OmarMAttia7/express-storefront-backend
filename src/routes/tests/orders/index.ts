ca9dca93168ced8dd9d9682f56c8a542b090ea09
hazel97 <omarmattia97@gmail.com>
2022-11-14 16:47:50 +0200

    test: Fixed wrong suite name

--- src/routes/tests/orders/index.ts
+++ src/routes/tests/orders/index.ts
@@ -2,7 +2,7 @@ import { setUpUsersRoute, tearDownUsersRoute } from "../test_utils/usersUtil";
 import getCurrentOrderSuite from "./getCurrentOrderSpec";
 import addOrderSuite from "./addOrder";
 function testSuite(): void {
-  describe("Users Routes :", () => {
+  describe("Orders Routes :", () => {
     beforeAll(async () => {
       await setUpUsersRoute();
     });
