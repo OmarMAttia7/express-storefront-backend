ac974ff951ca4fbdd714a4dc3a1ea18611c9f49e
hazel97 <omarmattia97@gmail.com>
2022-11-26 17:47:15 +0200

    fix: now handles migration fail correctly

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,10 @@ const port = normalizePort(env("APP_PORT"), 8080);
 const address = `0.0.0.0:${port}`;
 
 app.listen(port, async function () {
-  await dbmigrate.getInstance(true, { env: env("ENV") }).up();
+  await dbmigrate
+    .getInstance(true, { env: env("ENV") })
+    .up()
+    .catch((e: string) => console.log(`Could not connect to database.\n${e}`));
   await seed().catch(() => console.log("DB already seeded, no need to seed."));
   console.log(`starting app on ${address}`);
 });
