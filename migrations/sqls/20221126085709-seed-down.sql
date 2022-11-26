46ccf437401ff4f0b53f370e33154235417f0a14
hazel97 <omarmattia97@gmail.com>
2022-11-26 10:59:20 +0200

    chore: update migrations

---
+++ migrations/sqls/20221126085709-seed-down.sql
@@ -0,0 +1,5 @@
+DELETE * FROM currencies;
+DELETE * FROM statuses;
+DELETE * FROM categories WHERE category_name = 'Grocery';
+DELETE * FROM categories WHERE category_name = 'Sports';
+DELETE * FROM categories WHERE category_name = 'Technology';
